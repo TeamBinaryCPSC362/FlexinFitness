4b9574cc76ad8d5729aaef52c1578eeb1632e9a7
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-05-04 17:37:39 -0700

    Commit by Franco Arevalo

---
+++ app/src/main/java/com/example/flexinfitness/Calculators.java
@@ -0,0 +1,29 @@
+package com.example.flexinfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Calculators extends AppCompatActivity implements View.OnClickListener{
+
+    // declaring Views & ViewGroups
+    // declaring global data members
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculators);
+
+        // connecting views
+        // setting onclick listeners
+    } // end onCreate()
+
+    @Override
+    public void onClick(View v)
+    {
+        // switch(R.id.<switch conditional>)
+        //         ....
+    } // end onClick()
+} // end Calculators class
