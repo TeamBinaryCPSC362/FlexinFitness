034e3336d4a355166f54c54cff9257a995d0a858
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-05-04 22:19:46 -0700

    first part coding calculators

--- app/src/main/java/com/example/flexinfitness/Calculators.java
+++ app/src/main/java/com/example/flexinfitness/Calculators.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Calculators extends AppCompatActivity implements View.OnClickListener
+public class Calculators extends AppCompatActivity
 {
+    Button btn_calculate;
+
     // INPUTS
     TextView txtV_age;
     TextView txtV_weight;
     TextView txtV_height;
-    TextView txt_activityLevel;
+    TextView txtV_activityLevel;
 
     EditText edtxt_age;
     EditText edtxt_weight;
@@ -21,7 +24,17 @@ public class Calculators extends AppCompatActivity implements View.OnClickListen
     EditText edtxt_activityLevel;
 
     // OUTPUTS
+    TextView txtV_bmi;
+    TextView txtV_caloriesNeeded;
 
+    TextView txtV_bmiResult;
+    TextView txtV_caloriesNeededResult;
+
+    // DATA
+    String str_age;
+    String str_height;
+    String str_weight;
+    String str_activityLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,14 +42,32 @@ public class Calculators extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculators);
 
-        // connecting views
-        // setting onclick listeners
+        // connecting views & setting onclick listeners
+        btn_calculate = findViewById(R.id.btn_calculate);
+
+        txtV_age = findViewById(R.id.txtV_age);
+        txtV_weight = findViewById(R.id.txtV_weight);
+        txtV_height = findViewById(R.id.txtV_height);
+        txtV_activityLevel = findViewById(R.id.txtV_activityLevel);
+
+        edtxt_age = findViewById(R.id.edtxt_age);
+        edtxt_weight = findViewById(R.id.edtxt_weight);
+        edtxt_height = findViewById(R.id.edtxt_height);
+        edtxt_activityLevel = findViewById(R.id.edtxt_activityLevel);
+
+        txtV_bmi = findViewById(R.id.txtV_bmi);
+        txtV_caloriesNeeded = findViewById(R.id.txtV_caloriesNeeded);
+
+        txtV_bmiResult = findViewById(R.id.txtV_bmiResult);
+        txtV_caloriesNeededResult = findViewById(R.id.txtV_caloriesNeededResult);
     } // end onCreate()
 
-    @Override
-    public void onClick(View v)
+    public void calculate(View view)
     {
-        // switch(R.id.<switch conditional>)
-        //         ....
-    } // end onClick()
+        str_age = edtxt_age.getText().toString();
+        str_height = edtxt_height.getText().toString();
+        str_weight = edtxt_weight.getText().toString();
+        str_activityLevel = edtxt_activityLevel.getText().toString();
+    }
+
 } // end Calculators class
